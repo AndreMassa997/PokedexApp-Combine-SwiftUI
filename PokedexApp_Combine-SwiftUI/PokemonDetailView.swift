82f6577ca7dc4ea7f44e5737b3534626f1d51ce9
Andrea Massari <a.massari@reply.it>
2023-07-21 17:45:39 +0200

    Added CachedAsyncImage and Pokemon Detail View

---
+++ PokedexApp_Combine-SwiftUI/PokemonDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  PokemonDetailView.swift
+//  PokedexApp_Combine-SwiftUI
+//
+//  Created by Andrea Massari on 21/07/23.
+//
+
+import SwiftUI
+
+struct PokemonDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PokemonDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        PokemonDetailView()
+    }
+}
