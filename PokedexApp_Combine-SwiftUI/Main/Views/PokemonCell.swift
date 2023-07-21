d27d98d2045d80d3b0e8ef2e7094500ed5f004a6
Andrea Massari <a.massari@reply.it>
2023-07-21 17:46:12 +0200

    Refactor Pokemon Cell

--- PokedexApp_Combine-SwiftUI/Main/Views/PokemonCell.swift
+++ PokedexApp_Combine-SwiftUI/Main/Views/PokemonCell.swift
@@ -8,13 +8,44 @@
 import SwiftUI
 
 struct PokemonCell: View {
+    
+    let pokemon: PokemonModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct PokemonCell_Previews: PreviewProvider {
-    static var previews: some View {
-        PokemonCell()
+        ZStack{
+            RoundedRectangle(cornerRadius: 20)
+                .padding(EdgeInsets(top: 50, leading: 0, bottom: 0, trailing: 0))
+                .foregroundColor(Color(uiColor: pokemon.types?.first?.type.name.mainColor ?? .black))
+            VStack{
+                CachedAsyncImage(url: (pokemon.sprites?.other?.officialArtwork?.frontDefault ?? pokemon.sprites?.frontDefault) ?? URL(fileURLWithPath: "")){ phase in
+                    switch phase{
+                        case .success(let image):
+                            image.resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(height: 100)
+                        case .empty:
+                        ProgressView()
+                    case .failure(_):
+                        EmptyView()
+                            .frame(height: 100)
+                    @unknown default:
+                        EmptyView()
+                            .frame(height: 100)
+                    }
+                }
+                 
+                Text(pokemon.name.capitalized)
+                    .font(
+                        .system(size: 18)
+                        .weight(.black))
+                    .foregroundColor(.white)
+                Text(String.init(format: "#%03d", pokemon.id))
+                    .font(
+                        .system(size: 10)
+                        .weight(.light))
+                    .foregroundColor(.white)
+                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 10, trailing: 0))
+            }
+        }
     }
 }
