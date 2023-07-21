140ff0e42c33887c5104bda4d6b73c4b16d63f22
Andrea Massari <a.massari@reply.it>
2023-07-21 10:35:18 +0200

    First commit

---
+++ PokedexApp_Combine-SwiftUI/PokedexApp_Combine_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp_Combine_SwiftUIApp.swift
+//  PokedexApp_Combine-SwiftUI
+//
+//  Created by Andrea Massari on 21/07/23.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp_Combine_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
