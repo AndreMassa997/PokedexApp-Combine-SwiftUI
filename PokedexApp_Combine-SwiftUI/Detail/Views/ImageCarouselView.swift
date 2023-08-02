6564a43ff10253e22b73f3a0e38bfc1afb0675ab
Andrea Massari <a.massari@reply.it>
2023-08-02 15:51:55 +0200

    Added image carousel view

--- PokedexApp_Combine-SwiftUI/Detail/Views/ImageCarouselView.swift
+++ PokedexApp_Combine-SwiftUI/Detail/Views/ImageCarouselView.swift
@@ -11,32 +11,51 @@ struct ImageCarouselView: View {
     
     let imagesUrl: [URL]
     let color: UIColor
+    let name: String
+    let id: String
     
     var body: some View {
-        VStack{
-            TabView{
-                ForEach(imagesUrl, id: \.self){ url in
-                        CachedAsyncImage(url: url){ phase in
-                            switch phase{
-                                case .success(let image):
-                                    image.resizable()
+        ZStack{
+            RoundedCornersShape(corners: [.topRight, .topLeft], radius: 40)
+                .fill(.white)
+                .padding(.top, 120)
+            VStack{
+                TabView{
+                    ForEach(imagesUrl, id: \.self){ url in
+                            CachedAsyncImage(url: url){ phase in
+                                switch phase{
+                                    case .success(let image):
+                                        image.resizable()
+                                            .aspectRatio(contentMode: .fit)
+                                    case .empty:
+                                        ProgressView()
                                         .aspectRatio(contentMode: .fit)
-                                case .empty:
-                                    ProgressView()
-                                    .aspectRatio(contentMode: .fit)
-                                case .failure(_):
-                                    EmptyView()
-                                @unknown default:
-                                    EmptyView()
+                                    case .failure(_):
+                                        EmptyView()
+                                    @unknown default:
+                                        EmptyView()
+                                }
                             }
-                        }.padding(EdgeInsets(top: 0, leading: 0, bottom: 30, trailing: 0))
-                    
-                }
-            }.tabViewStyle(PageTabViewStyle())
+                            .frame(width: 200, height: 200)
+                    }
+                }.tabViewStyle(PageTabViewStyle())
+                
+                Text(name.capitalized)
+                    .font(
+                        .system(size: 30)
+                        .weight(.light))
+                    .foregroundColor(.black)
+                Text(id)
+                    .font(
+                        .system(size: 14)
+                        .weight(.thin))
+                    .foregroundColor(.black)
+            }
+            .onAppear{
+                setupAppearance()
+            }
         }
-        .onAppear{
-            setupAppearance()
-        }
+        
     }
     
     func setupAppearance() {
@@ -45,8 +64,29 @@ struct ImageCarouselView: View {
     }
 }
 
-struct ImageCarouselView_Previews: PreviewProvider {
-    static var previews: some View {
-        ImageCarouselView(imagesUrl: [], color: .clear)
+struct PageControl: UIViewRepresentable{
+    
+    var current: Int = 0
+    
+    func makeUIView(context: UIViewRepresentableContext<PageControl>) -> UIPageControl {
+        let page = UIPageControl()
+        return page
+    }
+    
+    func updateUIView(_ uiView: UIPageControl, context: Context) {
+        uiView.currentPage = current
+    }
+    
+}
+
+struct RoundedCornersShape: Shape {
+    let corners: UIRectCorner
+    let radius: CGFloat
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect,
+                                byRoundingCorners: corners,
+                                cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
     }
 }
